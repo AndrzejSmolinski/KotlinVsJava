66dbdf1ab07e93f8ab14e586d0741c44e544d051
Andrzej Smoliński <andrzej.smolinski@allegro.pl>
2023-11-25 18:41:45 +0100

    reworked ctor/inheritance example

--- src/main/kotlin/org/asmolinski/kotlin/InheritanceAndCtors.kt
+++ src/main/kotlin/org/asmolinski/kotlin/InheritanceAndCtors.kt
@@ -3,6 +3,33 @@ package org.asmolinski.kotlin
 import kotlin.random.Random
 import java.time.Instant
 
+class ClassicStyle {
+    private val member: String
+
+    constructor(member: String) {
+        this.member = member
+    }
+}
+
+class KotlinStyle(private val member: String)
+
+class SecondaryCtorAndInitBlock(val createdAt: Instant, val value: String) {
+    constructor(value: String): this(Instant.now(), value) {
+        println("odpalam secondary konstruktor")
+    }
+
+    private val liczbaLosowa: Int
+
+    init {
+        println("Inicjalizacja!!!!!")
+        liczbaLosowa = Random.nextInt()
+    }
+}
+
+fun main() {
+    SecondaryCtorAndInitBlock("wartość")
+}
+
 // Kotlin classes are "final" by default!
 class Closed
 
@@ -19,35 +46,10 @@ class Derived: Open(), Iface
 
 abstract class WithStringMember(val member: String)
 
-open class WithIntAndStringMember(stringMember: String, val intMember: Int): WithStringMember(stringMember)
+class WithIntAndStringMember(stringMember: String, val intMember: Int): WithStringMember(stringMember)
 
 fun demo() {
     val test = WithIntAndStringMember("jeden", 1)
     test.intMember
     test.member
-}
-
-class ClassicStyle {
-    private val member: String
-
-    constructor(member: String) {
-        this.member = member
-    }
-}
-
-class SecondaryCtorAndInitBlock(val createdAt: Instant, val value: String) {
-    constructor(value: String): this(Instant.now(), value) {
-         println("odpalam secondary konstruktor")
-    }
-
-    private val liczbaLosowa: Int
-
-    init {
-        println("Inicjalizacja!!!!!")
-        liczbaLosowa = Random.nextInt()
-    }
-}
-
-fun main() {
-    SecondaryCtorAndInitBlock("wartość")
 }
